717d28c2775e8bc89339f12b29319e6316855b25
ecazuc <ewen.cazz@gmail.com>
2023-11-22 09:07:20 +0100

    ajout script cswriter et scene menu

---
+++ Module Sante/Assets/Scripts/CsvWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class CsvWriter
+{
+     public string path = "/Resources/";
+     public string filename = "log";
+     
+     StringBuilder sb = new System.Text.StringBuilder();
+
+     public void addColnames(List<String> colnames)
+     {
+          sb.AppendLine(String.Join(";", colnames));
+     }
+     
+     public void record(List<String> values)
+     {
+          sb.AppendLine(String.Join(";", values));
+          SaveToFile(sb.ToString());
+     }
+     
+     public void SaveToFile(string content)
+     {
+          var folder = Application.dataPath + path;
+          if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+
+
+          var filePath = Path.Combine(folder, filename + ".csv");
+
+          using (var writer = new StreamWriter(filePath, false))
+          {
+               writer.Write(content);
+          }
+     }
+}
---
+++ Module Sante/Assets/Scripts/TestLog (1).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using System.IO;
+
+
+public class TestLog:MonoBehaviour
+{
+    private CsvWriter writer = new CsvWriter();
+    private float lastLog = 0;
+    
+    private void Start()
+    {
+        writer.filename = System.DateTime.Now.ToString("MM-dd-HH-mm-ss") + "-log";
+        writer.addColnames(new List<string>(){"time","pos_x","pos_y","pos_z"});
+    }
+
+    private void Update()
+    {
+        // log every 1sec
+        if (Time.time - lastLog > 1)
+        {
+            writer.record(new List<string>()
+            {
+                Time.time.ToString(),
+                transform.position.x.ToString(),
+                transform.position.y.ToString(),
+                transform.position.z.ToString()
+            });
+
+            lastLog = Time.time;
+        }
+
+    }
+    
+    private void OnApplicationQuit()
+    {
+        writer.record(new List<string>()
+        {
+            Time.time.ToString()
+        });
+    }
+}
